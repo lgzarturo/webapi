c01595d3b6ac1fff9bfbefb5f5ba6fc5a4ce2a48
Arturo López <lgzarturo@gmail.com>
2022-06-27 00:02:20 -0500

    test: singleton concurrente

--- src/test/java/com/arthurolg/patterns/creational/Singleton.java
+++ src/test/java/com/arthurolg/patterns/creational/Singleton.java
@@ -6,14 +6,26 @@ public class Singleton {
         System.out.println(connection.hashCode());
         var otherConnection = DbConnection.createConnection();
         System.out.println(otherConnection.hashCode());
+
+        Thread threadOne = new Thread(() -> {
+            var threadOneConnection = DbConnection.createConnection();
+            System.out.println("Thread 1: "+threadOneConnection.hashCode());
+        });
+
+        Thread threadTwo = new Thread(() -> {
+            var threadTwoConnection = DbConnection.createConnection();
+            System.out.println("Thread 2: "+threadTwoConnection.hashCode());
+        });
+
+        threadOne.start();
+        threadTwo.start();
     }
 }
 
 class DbConnection {
-
     private static DbConnection connection;
     private DbConnection() {}
-    public static DbConnection createConnection() {
+    public synchronized static DbConnection createConnection() {
         if (connection == null) {
             connection = new DbConnection();
         }
